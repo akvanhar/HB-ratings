d9b659a6cc493e14373fe2aa696a14b5c6a0fc03
Hackbright Student <info@hackbrightacademy.com>
2015-07-30 13:03:49 -0700

    creating our prediction algorithm

--- model.py
+++ model.py
@@ -27,9 +27,43 @@ class User(db.Model):
         """Provide helpful representation when printed."""
 
         return "<User user_id=%s email=%s>" % (self.user_id, self.email)
-        
 
+    def similarity(self, other):
+        """Return pearson rating for user compared to other user"""
 
+        u_ratings = {}
+        paired_ratings = []
+
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append((u_r.score, r.score))
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+
+        else:
+            return 0.0
+
+    def predict_rating(self, movie):
+        """Predict user's rating of a movie"""
+
+        other_ratings = movie.ratings
+        other_users = [r.user for r in other_ratings]
+
+        similarities = [
+        (self.similarity(other_user), other_user.user_id)
+        for other_user in other_users
+        ]
+
+        sim, best_match_id = max(similarities)
+
+        for rating in other_ratings:
+            if rating.user_id == best_match_id:
+                return rating.score * sim
 class Movie(db.Model):
     """Movies to be rated"""
 
